e4ee35e73761dd65d78aaf66cfe8d7c18eb8c04f
CharlieSinha00 <Charlie@FriedenthalFinancial.com>
2026-06-22 19:03:24 -0400

    Add tests for subtract, multiply, divide

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from main import subtract, multiply, divide
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+def test_divide():
+    assert divide(10, 2) == 5
