b0d93c7552b5d7ed0d68f78739c1e7cb24b79481
CharlieSinha00 <Charlie@FriedenthalFinancial.com>
2026-06-22 18:44:40 -0400

    Fix indentation on subtract, multiply, divide

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print("setup works")
 print("second change")
 def subtract(a,b):
-return a-b
+    return a-b
 def multiply(a,b):
-return a*b
+    return a*b
 def divide(a,b):
-return a/b
+    return a/b
